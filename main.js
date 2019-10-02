e56657c8e313a49d169d5c0e5958bb556ec23d65
Aaron <ajonaaronajon@yahoo.com>
2019-10-02 13:36:14 -0500

    Finished transitions between session timer and break timer

--- main.js
+++ main.js
@@ -1,3 +1,71 @@
+class Timer{
+  constructor(seconds){
+    this.milliseconds = seconds * 1000;
+    this.startMilliseconds = this.milliseconds;
+    this.timer;
+
+    this.running = false;
+    this.paused = false;
+    this.stopped = true;
+  
+  }
+  
+  
+  get time_s(){
+  return this.milliseconds / 1000;
+  }
+  get time_ms(){
+  return this.milliseconds;
+  }
+  set time_s(seconds){
+    this.milliseconds = seconds * 1000;
+  }
+  
+  set time_ms(milliseconds){
+  this.milliseconds = milliseconds;
+  }
+
+  start(/*element*/){
+    this.running = true;
+    this.paused = false;
+    this.stopped = false;
+    this.timer = setInterval(()=>{
+      if(this.milliseconds < 1)
+        this.stop();
+      else{
+        this.milliseconds -= 1000;
+        let minutes = Math.floor((this.milliseconds / 60000) % 60);
+        let seconds = (this.milliseconds / 1000) % 60
+        
+        //element.innerHTML = (minutes < 9 ? "0"+minutes : minutes) + ":" + (seconds < 9 ? "0"+seconds : seconds);
+
+      } 
+      
+    },1000);
+    
+  }
+  pause(){
+    clearInterval(this.timer);
+    this.running = false;
+    this.paused = true;
+    this.stopped = false;
+  }
+
+  stop(){
+    this.pause();
+    this.running = false;
+    this.paused = false;
+    this.stopped = true;
+    this.milliseconds = this.startMilliseconds;
+  }
+
+}
+
+const updateTimer = (element, timer)=> {
+  let minutes = Math.floor((timer.time_ms / 60000) % 60);
+  let seconds = (timer.time_ms / 1000) % 60
+  element.innerHTML = (minutes < 9 ? "0"+minutes : minutes) + ":" + (seconds <= 9 ? "0"+seconds : seconds);
+}
 
 const nightMode = document.querySelector('#night-mode');
 const body = document.querySelector('body');
@@ -41,35 +109,42 @@ nightMode.addEventListener('click',()=>{
   }
 });
 
-class Timer{
-  constructor(seconds){
-      this.milliseconds = seconds * 1000;
-      this.startMilliseconds = this.milliseconds;
-      this.timer;
-  }
 
-  get time(){
-      return this.milliseconds;
-  }
-  set time(seconds){
-    if(seconds > (60*25))
-      this.milliseconds = 60*25*1000;
-    else
-      this.milliseconds = seconds * 1000;
-  }
+let sessionTimer = new Timer(5);
+let breakTimer = new Timer(3);
+let newCycle = true; // so the breakTimer doesn't restart once it finishes it's cycle
+let activeTimer = '';
+//sessionTimer.start();
 
-  start(element){
-    this.timer = setInterval(()=>{
-      element.innerHTML = `${Math.floor(this.milliseconds / 60000)} : ${this.milliseconds/ 1000}`;
-      this.milliseconds -= 1000;
-    },1000);
+setInterval(()=>{
+  
+  if(activeTimer == 'session'){
+    if(sessionTimer.running){
+      updateTimer(clock, sessionTimer);
+    }else if (sessionTimer.stopped){
+      breakTimer.start();
+      activeTimer = 'break'
+    } 
+  } else if(activeTimer == 'break'){
+    if(breakTimer.running){
+      updateTimer(clock, breakTimer);
+    }else if (breakTimer.stopped){
+      sessionTimer.start();
+      activeTimer = 'session'
+    } 
+  }
+  /*if(sessionTimer.running){
+    updateTimer(clock, sessionTimer);
+  }else if (sessionTimer.stopped && breakTimer.stopped){
+    breakTimer.start();
+  } else if(breakTimer.running){
+    updateTimer(clock, breakTimer);
+  }else if(breakTimer.stopped){
+    sessionTimer.start();
+  }*/
     
-  }
+});
 
-}
-
-let timer = new Timer(60);
-timer.start(clock);
 
 
 
